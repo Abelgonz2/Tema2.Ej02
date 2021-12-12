1180d72c97c17a9f967eee23ae2ff1e2121a9a86
Abelgonz2 <abel.gonzalez.navarro@iescristobaldemonroy.es>
2021-12-12 23:28:57 +0100

    Se agrega el proyecto

---
+++ src/pruebas/HolaMundo.java
@@ -0,0 +1,10 @@
+package pruebas;
+
+public class HolaMundo {
+//Muestra en pantalla el mansaje "Hola Git!!"
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola GiT!!");
+	}
+
+}
